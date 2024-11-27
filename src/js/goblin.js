5fd347ec2e37889afebcc931d9139817246665cc
NadinDesyatova <nadindinina@yandex.ru>
2024-11-27 12:39:34 +0300

    Task 1 is completed

--- src/js/goblin.js
+++ src/js/goblin.js
@@ -1,3 +1,6 @@
+import goblinSrc from "../img/goblin.png";
+
+
 export default class Goblin {
   constructor() {
     this.class = "goblin-img";
@@ -6,6 +9,7 @@ export default class Goblin {
   createHtmlElement() {
     const goblin = document.createElement("img");
     goblin.setAttribute("class", this.class);
+    goblin.setAttribute("src", goblinSrc);
     return goblin;
   }
 }
